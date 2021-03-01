424e7f16030ff6eebd9aeb689bd86408cae79f09
Pascal van Buijtene <p_van_buijtene@hotmail.com>
2021-03-01 16:50:44 +0100

    Formating

--- client/client.go
+++ client/client.go
@@ -76,7 +76,7 @@ func (client *Client) Connect() error {
 			grpc.WithTransportCredentials(credentials.NewTLS(
 				&tls.Config{
 					InsecureSkipVerify: client.Config.SkipCertificateVerification,
-					RootCAs: client.Config.RootCAs,
+					RootCAs:            client.Config.RootCAs,
 				})))
 	}
 	opts = append(opts, grpc.WithPerRPCCredentials(basicAuth{
@@ -86,9 +86,9 @@ func (client *Client) Connect() error {
 
 	if client.Config.KeepAliveInterval >= 0 {
 		opts = append(opts, grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			Time:					client.Config.KeepAliveInterval,
-			Timeout:				client.Config.KeepAliveTimeout,
-			PermitWithoutStream:	true,
+			Time:                client.Config.KeepAliveInterval,
+			Timeout:             client.Config.KeepAliveTimeout,
+			PermitWithoutStream: true,
 		}))
 	}
 
@@ -224,7 +224,7 @@ func (client *Client) ReadAllEvents(context context.Context, direction direction
 }
 
 // SubscribeToStream ...
-func (client *Client) SubscribeToStream(context context.Context, streamID string, from uint64, resolveLinks bool, eventAppeared chan <- messages.RecordedEvent, checkpointReached chan<- position.Position, subscriptionDropped chan<- string) (*subscription.Subscription, error) {
+func (client *Client) SubscribeToStream(context context.Context, streamID string, from uint64, resolveLinks bool, eventAppeared chan<- messages.RecordedEvent, checkpointReached chan<- position.Position, subscriptionDropped chan<- string) (*subscription.Subscription, error) {
 	subscriptionRequest, err := protoutils.ToStreamSubscriptionRequest(streamID, from, resolveLinks, nil)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to construct subscription. Reason: %v", err)
@@ -252,7 +252,7 @@ func (client *Client) SubscribeToStream(context context.Context, streamID string
 }
 
 // SubscribeToAll ...
-func (client *Client) SubscribeToAll(context context.Context, from position.Position, resolveLinks bool, eventAppeared chan <- messages.RecordedEvent, checkpointReached chan<- position.Position, subscriptionDropped chan<- string) (*subscription.Subscription, error) {
+func (client *Client) SubscribeToAll(context context.Context, from position.Position, resolveLinks bool, eventAppeared chan<- messages.RecordedEvent, checkpointReached chan<- position.Position, subscriptionDropped chan<- string) (*subscription.Subscription, error) {
 	subscriptionRequest, err := protoutils.ToAllSubscriptionRequest(from, resolveLinks, nil)
 	readClient, err := client.streamsClient.Read(context, subscriptionRequest)
 	if err != nil {
@@ -273,7 +273,7 @@ func (client *Client) SubscribeToAll(context context.Context, from position.Posi
 }
 
 // SubscribeToAllFiltered ...
-func (client *Client) SubscribeToAllFiltered(context context.Context, from position.Position, resolveLinks bool, filterOptions filtering.SubscriptionFilterOptions, eventAppeared chan <- messages.RecordedEvent, checkpointReached chan<- position.Position, subscriptionDropped chan<- string) (*subscription.Subscription, error) {
+func (client *Client) SubscribeToAllFiltered(context context.Context, from position.Position, resolveLinks bool, filterOptions filtering.SubscriptionFilterOptions, eventAppeared chan<- messages.RecordedEvent, checkpointReached chan<- position.Position, subscriptionDropped chan<- string) (*subscription.Subscription, error) {
 	subscriptionRequest, err := protoutils.ToAllSubscriptionRequest(from, resolveLinks, &filterOptions)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to construct subscription. Reason: %v", err)
