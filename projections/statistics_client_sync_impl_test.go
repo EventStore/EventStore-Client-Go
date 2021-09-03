91c3b31da468a2b930323ac5f28e0bd0947a32c6
Radovan Zivkovic <pivonroll@gmail.com>
2021-09-03 14:57:06 +0200

    Add unit tests for  StatisticsClientSyncImpl

---
+++ projections/statistics_client_sync_impl_test.go
@@ -0,0 +1,47 @@
+package projections
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/EventStore/EventStore-Client-Go/protos/projections"
+	"github.com/golang/mock/gomock"
+)
+
+func TestStatisticsClientSyncImpl_Read(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("Result with no error returned from proto client", func(t *testing.T) {
+		protoResult := &projections.StatisticsResp{
+			Details: &projections.StatisticsResp_Details{
+				Name: "some name",
+			},
+		}
+
+		protoClient := projections.NewMockProjections_StatisticsClient(ctrl)
+		protoClient.EXPECT().Recv().Return(protoResult, nil)
+
+		statisticsClient := newStatisticsClientSyncImpl(protoClient)
+		result, err := statisticsClient.Read()
+
+		require.NoError(t, err)
+		require.Equal(t, StatisticsClientResponse{Name: "some name"}, result)
+	})
+
+	t.Run("Error returned from proto client", func(t *testing.T) {
+		errorResult := errors.New("some error")
+
+		protoClient := projections.NewMockProjections_StatisticsClient(ctrl)
+		protoClient.EXPECT().Recv().Return(nil, errorResult)
+
+		statisticsClient := newStatisticsClientSyncImpl(protoClient)
+		result, err := statisticsClient.Read()
+
+		require.Error(t, err)
+		require.EqualError(t, err, errorResult.Error())
+		require.Equal(t, StatisticsClientResponse{}, result)
+	})
+}
