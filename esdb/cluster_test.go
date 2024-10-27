01bfadfd20e8c4931fc60f066701711ae318c626
elhussein <a_elhussein@hotmail.com>
2024-10-27 18:11:31 +0300

    revert the test case

--- esdb/cluster_test.go
+++ esdb/cluster_test.go
@@ -2,8 +2,6 @@ package esdb_test
 
 import (
 	"context"
-	"fmt"
-	"os/exec"
 	"testing"
 	"time"
 
@@ -15,7 +13,6 @@ import (
 func ClusterTests(t *testing.T) {
 	t.Run("ClusterTests", func(t *testing.T) {
 		t.Run("notLeaderExceptionButWorkAfterRetry", notLeaderExceptionButWorkAfterRetry)
-		t.Run("readStreamAfterClusterRebalance", readStreamAfterClusterRebalance)
 	})
 }
 
@@ -59,62 +56,4 @@ func notLeaderExceptionButWorkAfterRetry(t *testing.T) {
 	}
 
 	t.Fatalf("we retried long enough but the test is still failing")
-}
-
-func readStreamAfterClusterRebalance(t *testing.T) {
-	// We purposely connect to a leader node.
-	db := CreateClient("esdb://admin:changeit@localhost:2111,localhost:2112,localhost:2113?tlsverifycert=false", t)
-	defer db.Close()
-	streamID := NAME_GENERATOR.Generate()
-
-	// Start reading the stream
-	options := esdb.ReadStreamOptions{From: esdb.Start{}}
-
-	stream, err := db.ReadStream(context.Background(), streamID, options, 10)
-	if err != nil {
-		t.Errorf("failed to read stream: %v", err)
-		return
-	}
-
-	stream.Close()
-
-	// Simulate node failure or rebalance
-	simulateClusterRebalance(t, db)
-
-	// Try reading the stream again
-	stream, err = db.ReadStream(context.Background(), streamID, options, 10)
-	if err == nil {
-		t.Errorf("unexpected to read stream after cluster rebalance: %v", err)
-		stream.Close()
-
-		return
-	}
-
-	// If we get an error, it means the client did not reconnect to the leader node.
-	// Wait for the client to reconnect to the leader node.
-	time.Sleep(5 * time.Second)
-
-	// Try reading the stream again
-	stream, err = db.ReadStream(context.Background(), streamID, options, 10)
-	if err != nil {
-		t.Errorf("failed to read stream after cluster rebalance: %v", err)
-		return
-	}
-
-	stream.Close()
-}
-
-func simulateClusterRebalance(t *testing.T, db *esdb.Client) {
-	// Stop one of the EventStoreDB nodes to simulate failure.
-	fmt.Println("Simulating cluster rebalance by stopping a node...")
-
-	stopNodeCmd := exec.Command("docker", "stop", "esdb-node1")
-
-	err := stopNodeCmd.Run()
-	if err != nil {
-		t.Fatalf("failed to stop node: %v", err)
-	}
-
-	// Wait for the leader to be elected.
-	WaitForLeaderToBeElected(t, db)
 }
